641afb03119473cce6133f23ddfdd86d61248039
PRINTEDESIGN <test@iMac-de-test.fritz.box>
2026-04-26 15:38:25 -0700

    Fix: add iosScheme capacitor + build 8

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,7 +5,8 @@ const config: CapacitorConfig = {
   appName: 'Hubbing',
   webDir: 'build',
   server: {
-    androidScheme: 'https'
+    androidScheme: 'https',
+    iosScheme: 'capacitor'
   }
 };
 
