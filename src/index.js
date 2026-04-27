3af432e95eb2095eaef3646445f0fa5bce0d2ae7
PRINTEDESIGN <test@iMac-de-test.fritz.box>
2026-04-26 19:18:54 -0700

    Debug: catch all errors + build 22

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-window.onerror = function(msg, src, line, col, error) {
-  document.body.innerHTML = '<div style="padding:20px;font-size:20px;color:red;background:white;">' + msg + '<br/>Line: ' + line + '</div>';
-  return false;
-};
+window.addEventListener('error', function(e) {
+  document.body.style.background = 'white';
+  document.body.innerHTML = '<div style="padding:20px;font-size:16px;color:red;">' + e.message + '<br/>' + e.filename + '<br/>Line: ' + e.lineno + '</div>';
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+window.addEventListener('unhandledrejection', function(e) {
+  document.body.style.background = 'white';
+  document.body.innerHTML = '<div style="padding:20px;font-size:16px;color:blue;">PROMISE: ' + e.reason + '</div>';
+});
 
-reportWebVitals();
+try {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<App />);
+} catch(e) {
+  document.body.style.background = 'white';
+  document.body.innerHTML = '<div style="padding:20px;font-size:16px;color:green;">CATCH: ' + e.message + '</div>';
+}
