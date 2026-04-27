e9c6290a8c03273e3dee1bbad2104915c315b86d
PRINTEDESIGN <test@iMac-de-test.fritz.box>
2026-04-26 18:55:34 -0700

    Debug: restore app with error handler + build 20

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+window.onerror = function(msg, src, line, col, error) {
+  document.body.innerHTML = '<div style="padding:20px;font-size:14px;color:red;">' + msg + '<br/>' + src + '<br/>Line: ' + line + '</div>';
+  return false;
+};
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div style={{background:'red', color:'white', fontSize:40, padding:50}}>
-    HUBBING FONCTIONNE
-  </div>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+reportWebVitals();
