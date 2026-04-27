e9a4f69bb94e8a97a5bd19994f9a52e30aa5e11b
PRINTEDESIGN <test@iMac-de-test.fritz.box>
2026-04-26 19:21:27 -0700

    Fix: clean index.js + build 23

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-window.addEventListener('error', function(e) {
-  document.body.style.background = 'white';
-  document.body.innerHTML = '<div style="padding:20px;font-size:16px;color:red;">' + e.message + '<br/>' + e.filename + '<br/>Line: ' + e.lineno + '</div>';
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-window.addEventListener('unhandledrejection', function(e) {
-  document.body.style.background = 'white';
-  document.body.innerHTML = '<div style="padding:20px;font-size:16px;color:blue;">PROMISE: ' + e.reason + '</div>';
-});
-
-try {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(<App />);
-} catch(e) {
-  document.body.style.background = 'white';
-  document.body.innerHTML = '<div style="padding:20px;font-size:16px;color:green;">CATCH: ' + e.message + '</div>';
-}
+reportWebVitals();
