3bf82ef21d5624aa68184a733d53207f3061eef5
PRINTEDESIGN <test@iMac-de-test.fritz.box>
2026-04-26 18:46:07 -0700

    Test: minimal React render + build 19

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-window.onerror = function(msg, src, line, col, error) {
-  document.body.innerHTML = '<div style="padding:20px;font-size:14px;color:red;">' + msg + '<br/>' + src + '<br/>Line: ' + line + '</div>';
-  return false;
-};
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div style={{background:'red', color:'white', fontSize:40, padding:50}}>
+    HUBBING FONCTIONNE
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
