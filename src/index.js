c7a8bcb7afbb365aee5bda45cce80f498edbba12
PRINTEDESIGN <test@iMac-de-test.fritz.box>
2026-04-26 19:04:20 -0700

    Debug: fix onerror position + build 21

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-window.onerror = function(msg, src, line, col, error) {
-  document.body.innerHTML = '<div style="padding:20px;font-size:14px;color:red;">' + msg + '<br/>' + src + '<br/>Line: ' + line + '</div>';
-  return false;
-};
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+window.onerror = function(msg, src, line, col, error) {
+  document.body.innerHTML = '<div style="padding:20px;font-size:20px;color:red;background:white;">' + msg + '<br/>Line: ' + line + '</div>';
+  return false;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
